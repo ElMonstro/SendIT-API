08295843f3de289e63141c52d778f5c51041c7b0
Elmonstro <jratcher@gmail.com>
2018-11-06 02:24:29 +0300

    Add test scenarios to cancel order tests

--- tests/v1/test_views.py
+++ tests/v1/test_views.py
@@ -31,18 +31,18 @@ class GoodRequest(ParcelsTestCase):
 
     def test_cancel_order(self):
         """Tests PUT /parcels/<id>/cancel"""
-        response = self.client.put('/parcels/350/cancel')
+        response = self.client.put('api/parcels/350/cancel')
         self.assertEqual(response.status_code, 204)
 
     def test_get_all_orders(self):
         """Tests GET /parcels"""
-        response = self.client.get('/parcels')
+        response = self.client.get('api/v1/parcels')
         self.assertEqual(response.status_code, 200)
         
 
     def test_get_all_orders_by_user(self):
         """Tests GET /users/<id>/parcels"""
-        response = self.client.get('/users/350/parcels')
+        response = self.client.get('api/v1/users/350/parcels')
         self.assertEqual(response.status_code, 200)
 
     def test_get_specific_order(self):
@@ -60,10 +60,17 @@ class BadRequest(ParcelsTestCase):
     #    pass
 
     def test_cancel_order(self):
-        """Tests bad requests with PUT /parcels/<id>/cancel"""
-        response = self.client.put('/parcels/35240/cancel') # Wrong parcel id
+        """Tests PUT /parcels/<id>/cancel"""
+        response = self.client.put('api/v1/parcels/35420/cancel') # Correct format but not there
+        data = json.loads(response.data)
+        self.assertEqual(response.status_code, 400)        
+        self.assertEqual(data, {'message': 'No Parcel delivery order with that id'})
+
+        response = self.client.put('api/v1/parcels/35uh420/cancel') # Incorrect id format
+        data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
-        self.assertEqual(response.data, {'message': 'No Parcel delivery order with that id'})
+        self.assertEqual(data, {'message': 'Wrong id format'})
+
         
 
     # def test_get_all_orders(self):
@@ -78,15 +85,17 @@ class BadRequest(ParcelsTestCase):
 
     def test_get_specific_order(self):
         """Tests bad requests with GET /parcels/<id>"""
-        response = self.client.get('api/v1/parcels/24034')
+        response = self.client.get('api/v1/parcels/24034')  # Correct format but not there
         data = json.loads(response.data)
         self.assertEqual(data, {'message': 'No Parcel delivery order with that id'})
         self.assertEqual(response.status_code, 400)
-        response = self.client.get('api/v1/parcels/24034u')
+
+        response = self.client.get('api/v1/parcels/24034u') # Incorrect id format
         data = json.loads(response.data)
         self.assertEqual(data, {'message': 'Wrong id format'})
         self.assertEqual(response.status_code, 400)
 
+    
 
 
 
