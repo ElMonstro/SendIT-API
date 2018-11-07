a5e6c199d0b7ca15a8bc2a6ee0b67bff416c5ab4
Elmonstro <jratcher@gmail.com>
2018-11-07 23:45:06 +0300

    Add authentication decorator and login handler implemented with jwt

--- app/api/v1/views.py
+++ app/api/v1/views.py
@@ -1,6 +1,32 @@
 from flask import request
 from flask_restful  import Resource
-from app.api.v1.models import ParcelOrders
+from app.api.v1.models import ParcelOrders, Validator, Users
+import jwt
+import datetime
+from instance.config import DevelopmentConfig
+from functools import wraps
+
+secret = DevelopmentConfig.SECRET
+
+
+
+message = 'message'
+
+# Authentication decorator
+def authenticate(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        if 'token' in request.headers:
+            token = request.headers.get('token')
+            try:
+                user_data = jwt.decode(token, key=secret)
+            except:
+                return {message: 'Invalid token'}
+            return f(*args, user_data, **kwargs)
+        else:
+            return {message: 'Token missing'}        
+        
+    return wrapper
 
 
 
@@ -10,8 +36,9 @@ class Parcels(Resource):
 
     def __init__(self):
         self.db = ParcelOrders()
-
-    def get(self):
+    
+    @authenticate
+    def get(self, user_data):
         return self.db.get_all_orders()
     
     def post(self):
@@ -20,9 +47,9 @@ class Parcels(Resource):
         success = self.db.save(data_list)
 
         if success:       
-            return {'messsage': 'Order created'}, 201
+            return {message: 'Order created'}, 201
         else:
-            return {'message': 'Invalid data format'}
+            return {message: 'Invalid data format'}
 
 
 class Parcel(Resource):
@@ -36,28 +63,28 @@ class Parcel(Resource):
         try:
             int_id = int(id)
         except:
-            return {'message': 'Wrong id format'}, 400
+            return {message: 'Wrong id format'}, 400
 
         order = self.db.get_specific_order(int_id)
 
         if order:
             return order
         else: 
-            return {'message': 'No Parcel delivery order with that id'}, 400
+            return {message: 'No Parcel delivery order with that id'}, 400
         
     
     def put(self, id):
         try:
             int_id = int(id)
         except:
-            return {'message': 'Wrong id format'}, 400
+            return {message: 'Wrong id format'}, 400
 
         success = self.db.change_delivery_status(int_id)
 
         if success:
-            return {'message': 'Status changed'} 
+            return {message: 'Status changed'} 
         else:       
-            return {'message': 'No Parcel delivery order with that id'}, 400
+            return {message: 'No Parcel delivery order with that id'}, 400
 
 
 class UserParcels(Resource):
@@ -70,12 +97,12 @@ class UserParcels(Resource):
         try:
             int_id = int(id)
         except:
-            return {'message': 'Wrong id format'}, 400
+            return {message: 'Wrong id format'}, 400
 
         orders = self.db.get_all_user_orders(int_id)
 
         if not  orders:
-            return {'message': 'No orders by that user'}, 400
+            return {message: 'No orders by that user'}, 400
         return orders
 
             
@@ -91,14 +118,46 @@ class CancelOrder(Resource):
         try:
             int_id = int(id)
         except:
-            return {'message': 'Wrong id format'}, 400
+            return {message: 'Wrong id format'}, 400
 
         success = self.db.cancel_order(int_id)
 
         if success:
-            return {'message': 'Order canceled'} 
+            return {message: 'Order canceled'} 
         else:       
-            return {'message': 'No Parcel delivery order with that id'}, 400
+            return {message: 'No Parcel delivery order with that id'}, 400
+
+class Login(Resource):
+    """Handles the route /login"""
+
+    def __init__(self):
+        self.auth = request.authorization
+        self.validator = Validator()
+        self.users = Users()
+
+    def post(self):
+        if self.auth:
+            email = self.auth.username
+            password = self.auth.password
+            user_id = self.validator.user_checker(email)
+            is_admin = self.users.is_admin(user_id)
+            # If user is not registered
+            if not user_id:
+                return {message: 'User email not found'}, 401
+
+            isValid = self.validator.password_checker(user_id, password)
+            # If password is valid
+            if isValid:
+                exp = datetime.datetime.utcnow() + datetime.timedelta(hours=24)
+                payload = {'user id': user_id, 'email': email,'is_admin': is_admin, 'exp': exp}
+                token = jwt.encode(payload, key=secret) 
+                return {message: 'You have been logged in',
+                'token': token.decode('utf-8')}
+            # If password not valid
+            return {message: 'Invalid password'}, 401
+        # If there is no authentication information
+        return {message: 'Email and password required'}, 401, {'www-Authenticate': 'Basic realm="Email and Password Please"'}
+
 
 
 
