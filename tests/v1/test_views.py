69aeda17e4ecac578defa4e985ab818b29676227
Elmonstro <jratcher@gmail.com>
2018-11-08 05:17:55 +0300

    Refractor code to accomodate authentication

--- tests/v1/test_views.py
+++ tests/v1/test_views.py
@@ -9,10 +9,14 @@ response_data = {"order": { "321": [532, "4 5345 343", "4 5343 343", 5, "Cancele
 
 users_orders = {
     "orders": {
-        "353": [350, "4 5435 324", "6 5356 353", 3, "Delivered" ],
-        "813": [350, "4 5435 324", "6 5356 353", 3,  "Delivered"]
+        "353": [103, "4 5435 324", "6 5356 353", 3, "Delivered" ],
+        "813": [103, "4 5435 324", "6 5356 353", 3,  "Delivered"]
     }
 }
+
+user_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIGlkIjoxMDMsImVtYWlsIjoiYWJieUBnbWFpbC5jb20iLCJpc19hZG1pbiI6ZmFsc2UsImV4cCI6MTU0MjI0NDM4NH0.wLLCb3qYi8ET1NgRj7oH9d9dhxN8hsJp3U81Rmjk4lA"
+admin_token =  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIGlkIjoxMDAsImVtYWlsIjoianJhdGNoZXJAZ21haWwuY29tIiwiaXNfYWRtaW4iOnRydWUsImV4cCI6MTU0MjI0NDQ4M30.q_JxTQ3FmDPMe01kIg8-aEJ4Tiik_J2FU39NjiLsazU"
+
 
 
 class ParcelsTestCase(unittest.TestCase):
@@ -24,6 +28,12 @@ class ParcelsTestCase(unittest.TestCase):
         self.client = self.app.test_client(self)
         self.app.testing = True
         self.order = order
+        admin_data = json.dumps({'email': 'jratcher@gmail.com',
+                                  'password': 'ulembaya'}) 
+
+        user_data = json.dumps({'email': 'dan@gmail.com',
+                                  'password': 'ulembaya'}) 
+
 
 
 class GoodRequestTestCase(ParcelsTestCase):
@@ -32,40 +42,46 @@ class GoodRequestTestCase(ParcelsTestCase):
     def test_create_order(self):
         """Tests POST /parcels"""
         response = self.client.post('/api/v1/parcels',
-                    data=json.dumps(self.order), content_type='application/json')
+                    data=json.dumps(self.order), content_type='application/json', headers={'token': user_token} )
+        
+        self.assertEqual(json.loads(response.data), {'message': 'Order created'} )
         self.assertEqual(response.status_code, 201)
-        self.assertEqual(json.loads(response.data), {'messsage': 'Order created'} )
+
 
     def test_admin_change_order_status(self):
         """Tests PUT /parcels/<id>"""
-        response = self.client.put('api/v1/parcels/321')
+        response = self.client.put('api/v1/parcels/321', headers={'token': admin_token})
         self.assertEqual(response.status_code, 200)
 
     def test_cancel_order(self):
         """Tests PUT /parcels/<id>/cancel"""
-        response = self.client.put('api/v1/parcels/321/cancel')
+        response = self.client.put('api/v1/parcels/321/cancel', headers={'token': user_token})
         self.assertEqual(response.status_code, 200)
 
 
     def test_get_all_orders(self):
         """Tests GET /parcels"""
-        response = self.client.get('api/v1/parcels')
+        response = self.client.get('api/v1/parcels', headers={'token': admin_token})
         self.assertEqual(response.status_code, 200)
         
 
     def test_get_all_orders_by_user(self):
         """Tests GET /users/<id>/parcels"""
-        response = self.client.get('api/v1/users/350/parcels')
+        response = self.client.get('api/v1/users/103/parcels', headers={'token': user_token})
         data = json.loads(response.data)
-        self.assertEqual(response.status_code, 200)
+        
         self.assertEqual(data, users_orders)
 
     def test_get_specific_order(self):
         """Tests GET /parcels/<id>"""
-        response = self.client.get('api/v1/parcels/321')
+        response = self.client.get('api/v1/parcels/321', headers={'token': user_token})
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(data, response_data)
+
+    def test_login(self):
+        """Tests Get /login"""
+        pass
 
 
 class BadRequestTestCase(ParcelsTestCase):
@@ -77,24 +93,24 @@ class BadRequestTestCase(ParcelsTestCase):
 
     def test_cancel_order(self):
         """Tests PUT /parcels/<id>/cancel"""
-        response = self.client.put('api/v1/parcels/35420/cancel') # Correct format but not there
+        response = self.client.put('api/v1/parcels/35420/cancel', headers={'token': user_token}) # Correct format but not there
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)        
         self.assertEqual(data, {'message': 'No Parcel delivery order with that id'})
 
-        response = self.client.put('api/v1/parcels/35uh420/cancel') # Incorrect id format
+        response = self.client.put('api/v1/parcels/35uh420/cancel', headers={'token': user_token}) # Incorrect id format
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data, {'message': 'Wrong id format'})
 
     def test_admin_change_order_status(self):
         """Tests bad requests to PUT /parcels/<id>"""
-        response = self.client.put('api/v1/parcels/35420') # Correct format but not there
+        response = self.client.put('api/v1/parcels/35420', headers={'token': admin_token}) # Correct format but not there
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)        
         self.assertEqual(data, {'message': 'No Parcel delivery order with that id'})
 
-        response = self.client.put('api/v1/parcels/35uh420') # Incorrect id format
+        response = self.client.put('api/v1/parcels/35uh420', headers={'token': admin_token}) # Incorrect id format
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data, {'message': 'Wrong id format'})
@@ -107,11 +123,11 @@ class BadRequestTestCase(ParcelsTestCase):
 
     def test_get_all_orders_by_user(self):
         """Tests bad requests to GET /users/<id>/parcels"""
-        response = self.client.get('api/v1/users/35530/parcels')
-        self.assertEqual(response.status_code, 400)
-        self.assertEqual(json.loads(response.data), {'message': 'No orders by that user'})
+        response = self.client.get('api/v1/users/35530/parcels', headers={'token': user_token})
+        self.assertEqual(response.status_code, 401)
+        self.assertEqual(json.loads(response.data), {'message': 'Cannot perform this operation'})
 
-        response = self.client.get('api/v1/users/35fsv530/parcels')
+        response = self.client.get('api/v1/users/35fsv530/parcels', headers={'token': user_token})
         data = json.loads(response.data)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(data, {'message': 'Wrong id format'})
@@ -119,15 +135,20 @@ class BadRequestTestCase(ParcelsTestCase):
 
     def test_get_specific_order(self):
         """Tests bad requests to GET /parcels/<id>"""
-        response = self.client.get('api/v1/parcels/24034')  # Correct format but not there
+        response = self.client.get('api/v1/parcels/24034', headers={'token': user_token})  # Correct format but not there
         data = json.loads(response.data)
         self.assertEqual(data, {'message': 'No Parcel delivery order with that id'})
         self.assertEqual(response.status_code, 400)
 
-        response = self.client.get('api/v1/parcels/24034u') # Incorrect id format
+        response = self.client.get('api/v1/parcels/24034u', headers={'token': user_token}) # Incorrect id format
         data = json.loads(response.data)
         self.assertEqual(data, {'message': 'Wrong id format'})
         self.assertEqual(response.status_code, 400)
+
+    def test_login(self):
+        """Tests bad requests to POST /login"""
+
+        pass
 
 
 class EdgeCasesTestCase(unittest.TestCase):
