aca65b685e67d4ffb67effd81c23b586d25dc023
Elmonstro <jratcher@gmail.com>
2018-11-16 00:58:52 +0300

    Register v2 blueprint

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,16 @@
+import os
 from app.api.v1 import v1
+from app.api.v2 import v2
 from flask import Flask
 from instance.config import DevelopmentConfig
+from app.db_config import create_tables, drop_tables
+
 
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(DevelopmentConfig)
+    create_tables(os.getenv('DB_URL'))
     app.register_blueprint(v1)
+    app.register_blueprint(v2)
     return app
---
+++ app/api/v2/views/__init__.py
@@ -0,0 +1,2 @@
+from .order_views import  Parcel, Parcels, CancelOrder
+from .auth_views import Login
