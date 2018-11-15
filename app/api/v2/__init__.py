f5e285f872649e835691f76723d49dac34b4e944
Elmonstro <jratcher@gmail.com>
2018-11-16 00:58:19 +0300

    Add resources

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,13 +1,13 @@
-from app.api.v1.views import Parcels, Parcel, CancelOrder, UserParcels, Login
-from flask import Blueprint
-from flask_restful import Api
+from app.api.v2.views import Parcels, Parcel, CancelOrder, Login
+from flask import Blueprint 
+from flask_restful import Api 
 
-v1 = Blueprint('v1', __name__, url_prefix='/api/v1')
-api = Api(v1)
+v2 = Blueprint('v2', __name__, url_prefix='/api/v2')
+api = Api(v2)
 
 
 api.add_resource(Parcels, '/parcels', strict_slashes=False)
 api.add_resource(Parcel, '/parcels/<id>', strict_slashes=False)
-api.add_resource(UserParcels, '/users/<id>/parcels', strict_slashes=False)
+#api.add_resource(UserParcels, '/users/<id>/parcels', strict_slashes=False)
 api.add_resource(CancelOrder, '/parcels/<id>/cancel', strict_slashes=False)
-api.add_resource(Login, '/login', strict_slashes=False)
+api.add_resource(Login, '/api/v2/auth/login', strict_slashes=False)
