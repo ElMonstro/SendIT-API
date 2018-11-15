1374b1a5da9d3b4640145d1e6fa4fbc7fd561f65
Elmonstro <jratcher@gmail.com>
2018-11-15 12:39:14 +0300

    Add  mock data

--- tests/v1/mock_data.py
+++ tests/v1/mock_data.py
@@ -1,15 +1,58 @@
 # Tests Mock data
 
 
-order = {'order': ['532', '4 5345 343', '4 5343 343', 5, 'In-transit']}
-response_data = {"order": { "321": [532, "4 5345 343", "4 5343 343", 5, "Canceled"]}}
-
-users_orders = {
-    "orders": {
-        "353": [103, "4 5435 324", "6 5356 353", 3, "Delivered" ],
-        "813": [103, "4 5435 324", "6 5356 353", 3,  "Delivered"]
-    }
+order = { 
+ "weight": 3,
+ "recepient_name": "Moracha",
+ "recepient_no": 726404622,
+ "pickup": 13456708,
+ "dest": 1234678 
 }
+
+bad_key = { 
+ "weight": 3,
+ "reepient_name": "Moracha",
+ "recepient_no": 726404622,
+ "pikup": 13456708,
+ "dest": 1234678 
+}
+
+less_keys = { 
+ "recepient_name": "Moracha",
+ "recepient_no": 726404622,
+ "pickup": 13456708,
+ "dest": 1234678 
+}
+
+invalid_data = { 
+ "weight": '3',
+ "recepient_name": "Moracha",
+ "recepient_no": 726404622,
+ "pickup": 13456708,
+ "dest": '1234678' 
+}
+
+invalid_tel = { 
+ "weight": 3,
+ "recepient_name": "Moracha",
+ "recepient_no": 72644622,
+ "pickup": 13456708,
+ "dest": 1234678 
+}
+
+invalid_addr = { 
+ "weight": 3,
+ "recepient_name": "Moracha",
+ "recepient_no": 726404622,
+ "pickup": 1356708,
+ "dest": 1234678 
+}
+
+
+
+
+
+
 
 # Login credentials
 admin_login = {'email': 'jratcher@gmail.com',
@@ -21,3 +64,17 @@ user_login = {'email': 'abby@gmail.com',
 message = 'message'
 
 expired_token =  "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIGlkIjoxMDMsImVtYWlsIjoiYWJieUBnbWFpbC5jb20iLCJpc19hZG1pbiI6ZmFsc2UsImV4cCI6MTU0MTc0MTE0MX0.uckKmwZ3YqQU4M36xhbEcXLx4KQ4B4Ej-Vua4Yw0HCM"
+
+
+
+mock_data = {
+    'order': order,
+    'expired': expired_token,
+    'admin': admin_login,
+    'user': user_login,
+    'bad_key': bad_key,
+    'less': less_keys,
+    'invalid_data': invalid_data,
+    'invalid_addr': invalid_addr,
+    'invalid_tel': invalid_tel
+}
