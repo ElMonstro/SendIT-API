468a0712ca46c15da71499178d3d5889ea1e3470
Elmonstro <jratcher@gmail.com>
2018-11-16 10:16:56 +0300

    Add admin role retriever

--- app/api/v2/utils/auth_decorator.py
+++ app/api/v2/utils/auth_decorator.py
@@ -2,6 +2,7 @@ import jwt
 import datetime
 from functools import wraps
 from flask import request, current_app as app
+from app.api.v2.models.user_models import Users
 
 message = 'message'
 
@@ -17,8 +18,12 @@ def authenticate(f):
                 return {message: 'Token expired, login again'}, 401
             except jwt.InvalidTokenError:
                 return {message: 'Invalid token'}, 401
-            
-            return f(*args, **kwargs, user_id=data['user_id'])
+            users = Users()
+            user_id=data['user_id']
+            is_admin = users.is_admin(user_id)
+            user_data = {'user_id': user_id, 'is_admin': is_admin}
+
+            return f(*args, **kwargs, user_data=user_data)
         else:
             return {message: 'Token missing'}, 401       
         
