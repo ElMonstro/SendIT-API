dcfca3dae52f3fc9b21b939bba2f8f08be979618
Elmonstro <jratcher@gmail.com>
2018-11-17 12:54:20 +0300

    Add user parcels resource

--- app/api/v2/__init__.py
+++ app/api/v2/__init__.py
@@ -1,4 +1,4 @@
-from app.api.v2.views import Parcels, Parcel, CancelOrder, Login
+from app.api.v2.views import Parcels, Parcel, CancelOrder, Login, UserParcels
 from flask import Blueprint 
 from flask_restful import Api 
 
@@ -8,6 +8,6 @@ api = Api(v2)
 
 api.add_resource(Parcels, '/parcels', strict_slashes=False)
 api.add_resource(Parcel, '/parcels/<id>', strict_slashes=False)
-#api.add_resource(UserParcels, '/users/<id>/parcels', strict_slashes=False)
+api.add_resource(UserParcels, '/users/<id>/parcels', strict_slashes=False)
 api.add_resource(CancelOrder, '/parcels/<id>/cancel', strict_slashes=False)
 api.add_resource(Login, '/auth/login', strict_slashes=False)
--- app/api/v2/views/__init__.py
+++ app/api/v2/views/__init__.py
@@ -1,2 +1,3 @@
 from .order_views import  Parcel, Parcels, CancelOrder
 from .auth_views import Login
+from .user_views import UserParcels
